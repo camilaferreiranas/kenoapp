6b5b81dda3b580f31c122fce7da242939a52f576
Camila Ferreira <camila_fn97@hotmail.com>
2026-03-08 13:21:21 -0300

    feat: commit inicial

---
+++ src/main/java/br/com/camilaferreiranas/kenoapp/KenoappApplication.java
@@ -0,0 +1,13 @@
+package br.com.camilaferreiranas.kenoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenoappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KenoappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/camilaferreiranas/kenoapp/KenoappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.camilaferreiranas.kenoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KenoappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
